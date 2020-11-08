73ad4e4fbc1d0e05a954e72aae1b4f0bcc3d0893
a-t-0 <eai0@protonmail.com>
2020-11-08 20:57:52 +0100

    Created basic report structure.

--- code/hw1/test/test_main.py
+++ code/hw1/test/test_main.py
@@ -16,17 +16,11 @@ class Test_main(unittest.TestCase):
     def get_script_dir(self):
         return os.path.dirname(__file__)
 
-    # tests unit test on main class
+    # tests unit test on addTwo function of main class 
     def test_addTwo(self):
         expected_result = 7
         result = self.main.addTwo(5)
         self.assertEqual(expected_result,result)
     
-    # Example: Verifies an exception is thrown in nqueens for n <5
-    #def test_ThreeQueens(self):
-    #   with self.assertRaises(Exception) as context:
-    #       n_queens = N_queens(4)
-    #    self.assertTrue('There are no non-trivial solutions for n<5, please increase n and try again.' in str(context.exception))
-        
 if __name__ == '__main__':
     unittest.main()
